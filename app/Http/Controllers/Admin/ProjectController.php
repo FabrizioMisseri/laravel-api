92fde9c7260ef529d1d7407066c1b11a88551a8a
FabrizioMisseri <misserifabrizio@gmail.com>
2023-01-19 14:41:49 +0100

    first commit

---
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreProjectRequest;
+use App\Http\Requests\UpdateProjectRequest;
+
+use App\Models\Project;
+use App\Models\Technology;
+use App\Models\Type;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $projects = Project::all();
+        return view('admin.projects.index', compact('projects'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $types = Type::all();
+        $technologies = Technology::all();
+        return view('admin.projects.create', compact('types', 'technologies'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreProjectRequest $request)
+    {
+        //dd($request);
+        $data = $request->validated();
+        $data['slug'] = Project::generateSlug($data['title']);
+        if ($request->hasFile('cover_image')) {
+            $path = Storage::put('projects_images', $request->cover_image);
+            $data['cover_image'] = $path;
+        }
+        // dd($data);
+        $project = Project::create($data);
+
+        // legare
+        if ($request->has('technologies')) {
+            $project->technologies()->attach($request->technologies);
+        }
+        //dd($project);
+        return redirect()->route('admin.projects.index')->with('message', 'Il project è sato creato con successo');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Project $project)
+    {
+        //dd($project->technologies);
+        return view('admin.projects.show', compact('project'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Project $project)
+    {
+        $technologies = Technology::all();
+        $types = Type::all();
+        return view('admin.projects.edit', compact('project', 'types', 'technologies'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateProjectRequest $request, Project $project)
+    {
+        $data = $request->validated();
+        $data['slug'] = Project::generateSlug($data['title']);
+        if ($request->hasFile('cover_image')) {
+            if ($project->cover_image) {
+                Storage::delete($project->cover_image);
+            }
+            $path = Storage::put('projects_images', $request->cover_image);
+            $form_data['cover_image'] = $path;
+        }
+        $project->update($data);
+
+        if ($request->has('technologies')) {
+            $project->technologies()->sync($request->technologies);
+        } else {
+            $project->technologies()->detach();
+        }
+
+        return redirect()->route('admin.projects.index')->with('message', "Il project '$project->slug' è sato modificato con successo");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Project $project)
+    {
+        $project->technologies()->detach();
+        if ($project->cover_image) {
+            Storage::delete($project->cover_image);
+        }
+        $project->delete();
+        return redirect()->route('admin.projects.index')->with('message', "Il project '$project->slug' è sato cancellato");
+    }
+}
